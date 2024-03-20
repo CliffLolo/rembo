dbf9d1f0a4bb4d78e0e53468a6c0101fb8bb276d
Clifford Frempong <cliffordfrempong@Cliffords-MacBook-Pro-3.local>
2024-03-20 06:16:08 +0000

    refactor clickhouse init.sql script

--- clickhouse/init.sql
+++ clickhouse/init.sql
@@ -1,22 +1,22 @@
 -- CREATE DATABASE IF NOT EXISTS rembo;
 CREATE TABLE IF NOT EXISTS rembo_data (
-    `before.employee_id` Nullable(UInt32),
+    `before.employee_id` UInt32,
     `before.employee_name` Nullable(String),
-    `before.employee_territory_id` Nullable(UInt32),
-    `before.territory_id` Nullable(UInt32),
+    `before.employee_territory_id` UInt32,
+    `before.territory_id` UInt32,
     `before.sales_territory_country` Nullable(String),
     `before.sales_territory_region` Nullable(String),
     `before.sales_territory_city` Nullable(String),
-    `before.customer_id` Nullable(UInt32),
+    `before.customer_id` UInt32,
     `before.last_name` Nullable(String),
     `before.address_line1` Nullable(String),
     `before.address_line2` Nullable(String),
-    `before.birth_date` Nullable(String),
+    `before.birth_date` Nullable(Date),
     `before.age` Nullable(UInt32),
     `before.commute_distance` Nullable(String),
     `before.customer_alternate_key` Nullable(String),
     `before.customer_key` Nullable(String),
-    `before.date_first_purchase` Nullable(String),
+    `before.date_first_purchase` Nullable(DateTime),
     `before.email_address` Nullable(String),
     `before.english_education` Nullable(String),
     `before.english_occupation` Nullable(String),
@@ -28,49 +28,50 @@ CREATE TABLE IF NOT EXISTS rembo_data (
     `before.marital_status` Nullable(String),
     `before.middle_name` Nullable(String),
     `before.name_style` Nullable(String),
-    `before.number_cars_owned` Nullable(String),
-    `before.number_children_at_home` Nullable(String),
+    `before.number_cars_owned` Nullable(UInt32),
+    `before.number_children_at_home` Nullable(UInt32),
     `before.phone` Nullable(String),
     `before.spanish_education` Nullable(String),
     `before.spanish_occupation` Nullable(String),
     `before.suffix` Nullable(String),
     `before.title` Nullable(String),
-    `before.total_children` Nullable(String),
-    `before.yearly_income` Nullable(String),
-    `before.sales_id` Nullable(UInt32),
+    `before.total_children` Nullable(UInt32),
+    `before.yearly_income` Nullable(Decimal64(2)),
+    `before.sales_id` UInt32,
     `before.currencykey` Nullable(String),
-    `before.client_id` Nullable(UInt32),
-    `before.discount_amount` Nullable(String),
-    `before.duedate` Nullable(String),
+    `before.client_id` UInt32,
+    `before.discount_amount` Decimal64(2),
+    `before.duedate` Nullable(DateTime),
     `before.duedatekey` Nullable(String),
-    `before.extended_amount` Nullable(String),
-    `before.freight` Nullable(String),
-    `before.order_date` Nullable(String),
-    `before.order_quantity` Nullable(String),
-    `before.product_standard_cost` Nullable(String),
+    `before.extended_amount` Nullable(Decimal64(2)),
+    `before.freight` Nullable(Decimal64(2)),
+    `before.order_date` Nullable(DateTime),
+    `before.order_quantity` Nullable(UInt32),
+    `before.product_standard_cost` Nullable(Decimal64(2)),
     `before.revision_number` Nullable(String),
-    `before.sales_amount` Nullable(String),
+    `before.sales_amount` Decimal64(2),
     `before.sales_order_line_number` Nullable(String),
-    `before.sales_order_number` Nullable(String),
-    `before.sales_territory_id` Nullable(UInt32),
-    `before.shipdate` Nullable(String),
-    `before.tax_amt` Nullable(String),
-    `before.total_product_cost` Nullable(String),
-    `before.unit_price` Nullable(String),
-    `before.unit_price_discount_pct` Nullable(String),
-    `before.sales_employee_id` Nullable(UInt32),
-    `after.employee_id` Nullable(UInt32),
+    `before.sales_order_number` Nullable(UInt32),
+    `before.sales_territory_id` UInt32,
+    `before.shipdate` Nullable(DateTime),
+    `before.tax_amt` Decimal64(2),
+    `before.total_product_cost` Decimal64(2),
+    `before.unit_price` Nullable(Decimal64(2)),
+    `before.unit_price_discount_pct` Nullable(Decimal64(2)),
+    `before.product_id` Nullable(UInt32),
+    `before.sales_employee_id` UInt32,
+    `after.employee_id` UInt32,
     `after.employee_name` Nullable(String),
-    `after.employee_territory_id` Nullable(UInt32),
-    `after.territory_id` Nullable(UInt32),
+    `after.employee_territory_id` UInt32,
+    `after.territory_id` UInt32,
     `after.sales_territory_country` Nullable(String),
     `after.sales_territory_region` Nullable(String),
     `after.sales_territory_city` Nullable(String),
-    `after.customer_id` Nullable(UInt32),
+    `after.customer_id` UInt32,
     `after.last_name` Nullable(String),
     `after.address_line1` Nullable(String),
     `after.address_line2` Nullable(String),
-    `after.birth_date` Nullable(String),
+    `after.birth_date` Nullable(Date),
     `after.age` Nullable(UInt32),
     `after.commute_distance` Nullable(String),
     `after.customer_alternate_key` Nullable(String),
@@ -95,73 +96,35 @@ CREATE TABLE IF NOT EXISTS rembo_data (
     `after.suffix` Nullable(String),
     `after.title` Nullable(String),
     `after.total_children` Nullable(UInt32),
-    `after.yearly_income` Nullable(String),
-    `after.sales_id` Nullable(UInt32),
+    `after.yearly_income` Nullable(Decimal64(2)),
+    `after.sales_id` UInt32,
     `after.currencykey` Nullable(String),
-    `after.client_id` Nullable(UInt32),
-    `after.discount_amount` Nullable(String),
-    `after.duedate` Nullable(String),
+    `after.client_id` UInt32,
+    `after.discount_amount` Decimal64(2),
+    `after.duedate` Nullable(DateTime),
     `after.duedatekey` Nullable(String),
-    `after.extended_amount` Nullable(String),
-    `after.freight` Nullable(String),
-    `after.order_date` Nullable(String),
-    `after.order_quantity` Nullable(String),
-    `after.product_standard_cost` Nullable(String),
+    `after.extended_amount` Nullable(Decimal64(2)),
+    `after.freight` Nullable(Decimal64(2)),
+    `after.order_date` Nullable(DateTime),
+    `after.order_quantity` Nullable(UInt32),
+    `after.product_standard_cost` Nullable(Decimal64(2)),
     `after.revision_number` Nullable(String),
-    `after.sales_amount` Nullable(String),
+    `after.sales_amount` UInt32,
     `after.sales_order_line_number` Nullable(String),
-    `after.sales_order_number` Nullable(String),
-    `after.sales_territory_id` Nullable(UInt32),
-    `after.shipdate` Nullable(String),
-    `after.tax_amt` Nullable(String),
-    `after.total_product_cost` Nullable(String),
-    `after.unit_price` Nullable(String),
-    `after.unit_price_discount_pct` Nullable(String),
-    `after.sales_employee_id` Nullable(UInt32),
+    `after.sales_order_number` Nullable(UInt32),
+    `after.sales_territory_id` UInt32,
+    `after.shipdate` Nullable(DateTime),
+    `after.tax_amt` Decimal64(2),
+    `after.total_product_cost` Decimal64(2),
+    `after.unit_price` Nullable(Decimal64(2)),
+    `after.unit_price_discount_pct` Nullable(Decimal64(2)),
+    `after.product_id` Nullable(UInt32),
+    `after.sales_employee_id` UInt32,
     `source.table` Nullable(String),
     op LowCardinality(String)
 ) ENGINE = MergeTree() 
 ORDER BY tuple();
 
-
-
-
-CREATE MATERIALIZED VIEW employee_mv
-TO employee_data
-AS
-SELECT
-    `after.employee_id` as employee_id,
-    `after.employee_name` as employee_name,
-    `after.employee_territory_id` as employee_territory_id
-FROM rembo_data
-WHERE `source.table` = 'employee';
-CREATE TABLE employee_data
-(
-    employee_id UInt32,
-    employee_name String,
-    employee_territory_id UInt32
-) ENGINE = ReplacingMergeTree()
-ORDER BY employee_id;
-
-
-CREATE MATERIALIZED VIEW sales_territory_mv
-TO sales_territory_data
-AS
-SELECT
-    `after.territory_id` as territory_id,
-    `after.sales_territory_country` as sales_territory_country,
-    `after.sales_territory_region` as sales_territory_region,
-    `after.sales_territory_city` as sales_territory_city
-FROM rembo_data
-WHERE `source.table` = 'sales_territory';
-CREATE TABLE sales_territory_data
-(
-    territory_id UInt32,
-    sales_territory_country String,
-    sales_territory_region String,
-    sales_territory_city String
-) ENGINE = ReplacingMergeTree()
-ORDER BY territory_id;
 
 
 CREATE MATERIALIZED VIEW customer_mv
@@ -206,12 +169,12 @@ CREATE TABLE customer_data
     last_name String,
     address_line1 String,
     address_line2 String,
-    birth_date String,
+    birth_date Date,
     age UInt32,
     commute_distance String,
     customer_alternate_key String,
     customer_key String,
-    date_first_purchase String,
+    date_first_purchase DateTime,
     email_address String,
     english_education String,
     english_occupation String,
@@ -223,17 +186,59 @@ CREATE TABLE customer_data
     marital_status String,
     middle_name String,
     name_style String,
-    number_cars_owned String,
-    number_children_at_home String,
+    number_cars_owned UInt32,
+    number_children_at_home UInt32,
     phone String,
     spanish_education String,
     spanish_occupation String,
     suffix String,
     title String,
-    total_children String,
-    yearly_income String
+    total_children UInt32,
+    yearly_income Decimal64(2)
 ) ENGINE = ReplacingMergeTree()
 ORDER BY customer_id;
+
+
+CREATE MATERIALIZED VIEW sales_territory_mv
+TO sales_territory_data
+AS
+SELECT
+    `after.territory_id` as territory_id,
+    `after.sales_territory_country` as sales_territory_country,
+    `after.sales_territory_region` as sales_territory_region,
+    `after.sales_territory_city` as sales_territory_city
+FROM rembo_data
+WHERE `source.table` = 'sales_territory';
+CREATE TABLE sales_territory_data
+(
+    territory_id UInt32,
+    sales_territory_country String,
+    sales_territory_region String,
+    sales_territory_city String
+) ENGINE = ReplacingMergeTree()
+ORDER BY territory_id;
+
+
+
+CREATE MATERIALIZED VIEW employee_mv
+TO employee_data
+AS
+SELECT
+    `after.employee_id` as employee_id,
+    `after.employee_name` as employee_name,
+    `after.employee_territory_id` as employee_territory_id
+FROM rembo_data
+WHERE `source.table` = 'employee';
+CREATE TABLE employee_data
+(
+    employee_id UInt32,
+    employee_name String,
+    employee_territory_id UInt32
+) ENGINE = ReplacingMergeTree()
+ORDER BY employee_id;
+
+
+
 
 
 CREATE MATERIALIZED VIEW sales_mv
@@ -261,6 +266,7 @@ SELECT
     `after.total_product_cost` as total_product_cost,
     `after.unit_price` as unit_price,
     `after.unit_price_discount_pct` as unit_price_discount_pct,
+    `after.product_id` as product_id,
     `after.sales_employee_id` as sales_employee_id
 FROM rembo_data
 WHERE `source.table` = 'sales';
@@ -269,24 +275,25 @@ CREATE TABLE sales_data
     sales_id UInt32,
     currencykey String,
     client_id UInt32,
-    discount_amount String,
-    duedate String,
+    discount_amount Decimal64(2),
+    duedate DateTime,
     duedatekey String,
-    extended_amount String,
-    freight String,
-    order_date String,
-    order_quantity String,
-    product_standard_cost String,
+    extended_amount Decimal64(2),
+    freight Decimal64(2),
+    order_date DateTime,
+    order_quantity UInt32,
+    product_standard_cost Decimal64(2),
     revision_number String,
-    sales_amount String,
+    sales_amount Decimal64(2),
     sales_order_line_number String,
-    sales_order_number String,
+    sales_order_number UInt32,
     sales_territory_id UInt32,
-    shipdate String,
-    tax_amt String,
-    total_product_cost String,
-    unit_price String,
-    unit_price_discount_pct String,
+    shipdate DateTime,
+    tax_amt Decimal64(2),
+    total_product_cost Decimal64(2),
+    unit_price Decimal64(2),
+    unit_price_discount_pct Decimal64(2),
+    product_id UInt32,
     sales_employee_id UInt32
 ) ENGINE = ReplacingMergeTree()
 ORDER BY sales_id;
